1fbee96178f6ec9aca04fcb596b8185fdfe4e740
Dylan Weicker <dylanweicker@gmail.com>
2018-04-23 17:11:18 -0700

    Work after first consult with dad

--- ggi/script.js
+++ ggi/script.js
@@ -1,8 +1,25 @@
 function toggleHiddenMenu(){
+    if (window.innerWidth < 893){
+        toggleDropDownMenu();
+    }
+    else{
+        openLongMenu();
+    }
+}
+
+function toggleDropDownMenu(){
     var hiddenMenu = document.getElementById("hidden-menu");
     if (hiddenMenu.style.display === 'none') {
         hiddenMenu.style.display = 'block';
     } else {
         hiddenMenu.style.display = 'none';
     }
+}
+
+function openLongMenu(){
+    var longNav = document.getElementById("long-nav");
+    var sandwichBtn = document.getElementById("sandwich-btn");
+    longNav.style.display = 'block';
+    sandwichBtn.style.display = 'none';
+        
 }

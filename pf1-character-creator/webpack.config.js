c7032bb796a38827db20c265825e575eb0c47dea
Dylan Weicker <dylanweicker@gmail.com>
2020-01-28 10:12:57 -0800

    changing public path directory

--- pf1-character-creator/webpack.config.js
+++ pf1-character-creator/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/pf1-character-creator/dist/',
     filename: 'build.js'
   },
   module: {
